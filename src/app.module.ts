99a6b444fa7c5e382b25380010c13ed6feb5081b
tunagch190847 <anhtungo159@gmail.com>
2023-01-12 15:18:57 +0700

    build database app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      // imports: [ConfigModule],
+      // inject: [ConfigService],
+      useFactory: () => ({
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: 'n1nbmkbhgct',
+        database: 'almira',
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+        autoLoadEntities: true,
+        logging: true,
+        // logger: new DatabaseMysqlLogger(),
+        timezone: '+09:00',
+        legacySpatialSupport: false, //fix version mysql 8
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
